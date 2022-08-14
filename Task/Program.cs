f000977b57e54aec0d1a4c8258c1124aa0b897fd
Vasiliy <vshehlov@gmail.com>
2022-08-14 19:24:05 +0300

    End

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Clear();
-string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array1 = new string[5] {"Bob", "Li", "Hiser", "Dolf", "Noy"};
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
